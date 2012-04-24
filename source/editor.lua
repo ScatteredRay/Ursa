b7f54950508bad76bb8d3f439e57d86c9dd4ee8a
Nicholas 'Indy' Ray <arelius@gmail.com>
2012-04-23 23:18:41 -0700

    Additional Win32 fixes.
    * Moved OSX specific frameworks into a platform specific condition.

--- source/editor.lua
+++ source/editor.lua
@@ -2,7 +2,10 @@ project("editor")
     kind("WindowedApp")
     files { "Editor/src/**.h", "Editor/src/**.cpp" }
     includedirs { "Irrlicht/inc", "lua/inc", "luasocket/inc" }
-    links { "irrlicht", "lua", "luasocket", "OpenGL.framework", "Cocoa.framework", "IOKit.framework" }
+    links { "irrlicht", "lua", "luasocket" }
+    if(os.get() == "macosx") then
+       links { "OpenGL.framework", "Cocoa.framework", "IOKit.framework" }
+    end
 
     configuration("Debug")
         defines { "_DEBUG" }
